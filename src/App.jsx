f96a4384603b58c27490733eae622d7d16e9b997
Hritika Prasad <hritikaprasad15@gmail.com>
2025-06-25 23:30:57 +0530

    Lottery Game using React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Lottery from "./Lottery";
+
+
+function App() {
+
+return (
+  <>
+  <Lottery/>
+  </>
+);
+
+}
+
+export default App
---
+++ src/Lottery.jsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+
+import {genTicket,sum} from "./helper";
+import Ticket from "./Ticket";
+
+export default function Lottery({n=3,winningSum=15}){
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket)===winningSum;
+
+    let buyTicket = ()=>{
+        setTicket(genTicket(n));
+    }
+   return(
+    <div>
+        <h1>LOTTERY GAME!</h1>
+       <Ticket ticket={ticket}/>
+       <br/>
+        <button onClick={buyTicket}>Buy New Ticket</button>
+        <h3>{isWinning && "Congratulations, you won!"}</h3>
+    </div>
+   ); 
+}
